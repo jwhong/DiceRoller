173d7658b6376d3e0c7bcd3c24420982f51d760a
James-Thinkpad-P17 <james.whong@gmail.com>
2023-08-05 13:02:33 -0600

    Refactor filenames

--- interpreter.py
+++ interpreter.py
@@ -1,162 +1,214 @@
-from typing import *
-import random
+import re
+from graph import makeBarGraph
+from numpy import random
 
-def xDy(x, y)->List[int]:
-    return [random.randint(1, y) for i in range(x)]
-
-def process(script, starting_pool=None, indent_level=0)->List[int]:
-    tail = script
-
-    loaded_int = None
-    loaded_subpool = None
-    pool = [] if starting_pool is None else starting_pool
-
-    def iPrint(*args):
-        print('    '*indent_level, *args)
-
-    def printPool():
-        iPrint("Pool has %d dice, sums to %d"%(len(pool), sum(pool)))
-        iPrint(pool)
-
-    def cutHeadWhitespace(s):
-        for i in range(len(s)):
-            if not s[i].isspace():
-                return s[i:]
-        return ''
-
-    def grabMatching(s, start_delim, end_delim):
-        # We are assuming s starts immediately AFTER the first start delim
-        # Returns the block of text from the start of s to the ending delim, and the tail after the ending delim.
-        # Ending delim is lost.
-        nest_count = 1
-        for i in range(len(s)):
-            if s[i] == start_delim:
-                nest_count += 1
-            if s[i] == end_delim:
-                nest_count -= 1
-                if nest_count == 0:
-                    return s[:i], s[i+1:]
-        raise Exception("Ending delimiter for %s not found!"%start_delim)
-
-    def popInteger(s):
-        # Check special case arguments first
-        if len(s) == 0:
-            return None, ''
-        if s[0].upper() == 'S':
-            return sum(pool), s
-        if s[0].upper() == 'C':
-            return len(pool), s
-        i = 0
-        while i < len(s) and s[i].isdigit():
-            i += 1
-        integer_part = s[:i]
-        s = s[i:]
-        return int(integer_part), s
-
-    while tail:
-        c = tail[0]
-        tail = tail[1:]
-        if c.isspace():
-            continue
-        if c.isdigit():
-            assert (loaded_int is None)
-            loaded_int, tail = popInteger(c+tail)
-            continue
-        if c.upper() == 'D':
-            n_sides, tail = popInteger(tail)
-            assert(loaded_int is not None)
-            pool = xDy(loaded_int, n_sides)
-            loaded_int = None
-            continue
-        if c == '<':
-            thresh, tail = popInteger(cutHeadWhitespace(tail))
-            pool = list(filter(lambda x: x < thresh, pool))
-            continue
-        if c == '>':
-            thresh, tail = popInteger(cutHeadWhitespace(tail))
-            pool = list(filter(lambda x: x > thresh, pool))
-            continue
-        if c == '*':
-            mult, tail = popInteger(cutHeadWhitespace(tail))
-            pool *= mult
-            continue
-        if c.upper() == 'S':
-            assert(loaded_int is None)
-            loaded_int = sum(pool)
-            continue
-        if c.upper() == 'C':
-            assert (loaded_int is None)
-            loaded_int = len(pool)
-            continue
-        if c.upper() == 'P':
-            printPool()
-            continue
-        if c == '{':
-            subscript, tail = grabMatching(tail, '{', '}')
-            poolarg = None
-            if loaded_subpool is None:
-                poolarg = pool
+class Interpreter:
+    ws_re  = r'\s+'
+    int_re = r'\d+'
+    s_re   = r'S'
+    c_re   = r'C'
+    end_curly_re = r'\}'
+    end_square_re = r'\]'
+    def __init__(self, script, pool=None, end_re=None, nest_level=None):
+        # Purge all comments from the script
+        script = ''.join(re.split(r'#.*\n', script+'\n'))
+        self.original_script = script
+        self.script_tail = script
+        self.pool = pool if pool else []
+        self.arg_stack = [] # Stack of integers
+        self.end_re = end_re if end_re else None
+        self.nest_level = nest_level if nest_level is not None else 0
+        self.verbose = 0
+    def print(self, *args):
+        if not self.verbose:
+            return
+        print(self.nest_level * '    ', *args)
+    def advanceByMatch(self, exp)->str:
+        matching = re.match(exp, self.script_tail)
+        if matching is None:
+            return None
+        matching = matching.group()
+        rval = self.script_tail[:len(matching)]
+        self.script_tail = self.script_tail[len(matching):]
+        return rval
+    def grabPostFixArgument(self):
+        # This is kinda ugly b/c we need to break the order of interpretation...
+        self.advanceByMatch(self.ws_re)
+        # The only valid post-fix values are integer literals, S or C
+        if matched := self.advanceByMatch(self.int_re):
+            return int(matched)
+        if self.advanceByMatch(self.s_re):
+            return sum(self.pool)
+        if self.advanceByMatch(self.c_re):
+            return len(self.pool)
+        return None
+    def doWS(self):
+        return self.advanceByMatch(self.ws_re)
+    def doInt(self):
+        matched = self.advanceByMatch(self.int_re)
+        if matched:
+            self.arg_stack.append(int(matched))
+        return matched
+    def doS(self):
+        matched = self.advanceByMatch(self.s_re)
+        if matched:
+            self.arg_stack.append(sum(self.pool))
+            self.print("Sum is %d" % self.arg_stack[0])
+        return matched
+    def doC(self):
+        matched = self.advanceByMatch(self.c_re)
+        if matched:
+            self.arg_stack.append(len(self.pool))
+            self.print("Count is %d" % self.arg_stack[0])
+        return matched
+    def doD(self):
+        matched = self.advanceByMatch(r'D')
+        if matched:
+            n_dice  = self.arg_stack.pop()
+            n_sides = self.grabPostFixArgument()
+            self.pool = list(random.randint(1, n_sides+1, n_dice))
+            self.print("Rolled %dD%d, got:"%(n_dice, n_sides))
+            self.print(self.pool)
+        return matched
+    def doPlus(self):
+        matched = self.advanceByMatch(r'\+')
+        if matched:
+            # Is this a "X+" filter or a "+X" appending to the pool?
+            if len(self.arg_stack):
+                # This is an X+
+                thresh = self.arg_stack.pop()
+                to_remove = list(filter(lambda x: x < thresh, self.pool))
+                for x in to_remove:
+                    self.pool.remove(x)
+                # This is basically a 2nd argument register... Maybe find a more elegant way
+                self.subpool = to_remove
+                self.print("Pass on %d+"%thresh)
+                self.print("Removed %d dice, leaving %d"%(len(to_remove),len(self.pool)))
             else:
-                poolarg = loaded_subpool
-                loaded_subpool = None
-            reparg = 1
-            if loaded_int is not None:
-                reparg = loaded_int
-                loaded_int = None
-            for i in range(reparg):
-                pool.extend(process(subscript, poolarg.copy(), indent_level+1))
-            continue
-        if c == '[':
-            subscript, tail = grabMatching(tail, '[', ']')
-            assert(loaded_subpool is None)
-            loaded_subpool = process(subscript, pool.copy(), indent_level+1) # ONLY USE FILTERS IN THIS SUBSCRIPT
-            for x in loaded_subpool:
-                pool.remove(x)
-            continue
-        if c == '+':
-            # Meaning is contextual, depends if it's a "+2" or "2+"
-            if loaded_int is None:
-                int_arg, tail = popInteger(cutHeadWhitespace(tail))
-                pool = [x+int_arg for x in pool]
+                # This is a +X
+                to_append = self.grabPostFixArgument()
+                self.pool.append(to_append)
+                self.print("Added +%d to pool"%to_append)
+        return matched
+    def doMinus(self):
+        matched = self.advanceByMatch(r'\-')
+        if matched:
+            if len(self.arg_stack):
+                thresh = self.arg_stack.pop()
+                to_remove = list(filter(lambda x: x > thresh, self.pool))
+                for x in to_remove:
+                    self.pool.remove(x)
+                # This is basically a 2nd argument register... Maybe find a more elegant way
+                self.subpool = to_remove
+                self.print("Pass on %d-" % thresh)
+                self.print("Removed %d dice, leaving %d"%(len(to_remove),len(self.pool)))
             else:
-                pool = list(filter(lambda x: x >= loaded_int, pool))
-                loaded_int = None
-            continue
-        if c == '-':
-            # Meaning is contextual, depends if it's a "-2" or "2-"
-            if loaded_int is None:
-                int_arg, tail = popInteger(cutHeadWhitespace(tail))
-                pool = [x-int_arg for x in pool]
-            else:
-                pool = list(filter(lambda x: x <= loaded_int, pool))
-                loaded_int = None
-            continue
-        if c == '#':
-            # Comment
-            comment, tail = grabMatching(tail, None, '\n')
-            iPrint("#", comment)
-            printPool()
-            continue
-        if c.upper() == 'H':
-            assert(loaded_int is None)
-            int_arg, tail = popInteger(cutHeadWhitespace(tail))
-            pool = sorted(pool)[-int_arg:]
-            continue
-        if c.upper() == 'L':
-            assert (loaded_int is None)
-            int_arg, tail = popInteger(cutHeadWhitespace(tail))
-            pool = sorted(pool)[:int_arg]
-            continue
-        if c.upper() == 'A':
-            assert (loaded_int is None)
-            int_arg, tail = popInteger(cutHeadWhitespace(tail))
-            pool.append(int_arg)
-            continue
-    # Special case: The user has loaded up an argument before the end of the script, we assume they want that returned
-    # instead of the pool.
-    if loaded_int is not None:
-        return [loaded_int]
-    return pool
+                # This is a -X
+                to_append = self.grabPostFixArgument()
+                to_append *= -1
+                self.pool.append(to_append)
+                self.print("Added %d to pool" % to_append)
+        return matched
+    def doMult(self):
+        matched = self.advanceByMatch(r'\*')
+        if matched:
+            factor = self.grabPostFixArgument()
+            self.pool *= factor
+            self.print("Multiplied pool by %d, pool has %d elements"%(factor, len(self.pool)))
+        return matched
+    def doH(self):
+        matched = self.advanceByMatch(r'H')
+        if matched:
+            count = self.grabPostFixArgument()
+            self.pool = sorted(self.pool)[-count:]
+            self.print("Grabbed %d highest elements"%count)
+        return matched
+    def doL(self):
+        matched = self.advanceByMatch(r'L')
+        if matched:
+            count = self.grabPostFixArgument()
+            self.pool = sorted(self.pool)[:count]
+            self.print("Grabbed %d lowest elements" % count)
+        return matched
+    def doCurlyBracket(self, pool_override=None):
+        matched = self.advanceByMatch(r'{')
+        if matched:
+            repeat_count = self.arg_stack.pop() if len(self.arg_stack) else 1
+            new_tail = None
+            new_entries = []
+            for i in range(repeat_count):
+                self.print("{} run %d"%i)
+                sub = Interpreter(self.script_tail,
+                                  pool_override.copy() if pool_override is not None else self.pool.copy(),
+                                  self.end_curly_re, self.nest_level+1)
+                sub.run()
+                new_tail = sub.script_tail
+                # By convention: If the sub left arguments in its stack, append those to the pool.
+                # Otherwise, append the subpool to the pool.
+                if len(sub.arg_stack):
+                    new_entries.extend(sub.arg_stack)
+                else:
+                    new_entries.extend(sub.pool)
+            self.print("{} finished, adding %d new entries:"%len(new_entries))
+            self.print(new_entries)
+            self.pool.extend(new_entries)
+            self.subpool = None
+            self.script_tail = new_tail
+        return matched
+    def doSquareBracket(self):
+        matched = self.advanceByMatch(r'\[')
+        if matched:
+            self.print("[] starting")
+            sub = Interpreter(self.script_tail,
+                              self.pool.copy(),
+                              self.end_square_re, self.nest_level+1)
+            sub.run()
+            self.script_tail = sub.script_tail
 
-#test_script = "10D3 P SD6 P [1-]{CD6} [6+]{*3}  P 5+ CD6 P 5+ P"
-process(open('test.txt').read())
+            # Remove all elements of the subpool from the main pool
+            for x in sub.pool:
+                self.pool.remove(x)
+            self.print("[] finished, subpool has %d entries:" % len(sub.pool))
+            self.print(sub.pool)
+
+            # If there is a curly block after this, we need to pass it the sub pool
+            self.doWS()
+            self.doCurlyBracket(sub.pool)
+        return matched
+    def doG(self):
+        matched = self.advanceByMatch(r'G')
+        if matched:
+            self.print("Graphing...")
+            makeBarGraph(self.pool)
+        return matched
+    def run(self):
+        keyword_checkers = [
+            self.doWS,
+            self.doInt,
+            self.doS,
+            self.doC,
+            self.doD,
+            self.doPlus,
+            self.doMinus,
+            self.doCurlyBracket,
+            self.doSquareBracket,
+            self.doMult,
+            self.doH,
+            self.doL,
+            self.doG,
+        ]
+        while len(self.script_tail):
+            found_keyword = False
+            for checker in keyword_checkers:
+                if checker():
+                    found_keyword = True
+                    break
+            if found_keyword: continue
+            if self.end_re and self.advanceByMatch(self.end_re):
+                # We found the end condition, we rejoin the parent
+                break
+            raise SyntaxError("Can't find a valid next token!")
+
+if 1:
+    inter = Interpreter(open('test.txt').read())
+    inter.run()
